7c5fca4aad63e3b85e27cbcf437d9155b6662739
Muhammad Ahtisham <ahtishamn898@gmail.com>
2026-05-30 21:13:47 +0500

    new changes in footer

--- wp-content/themes/my-side/footer.php
+++ wp-content/themes/my-side/footer.php
@@ -1,7 +1,7 @@
     </div><!-- #content -->
 
     <footer id="colophon" class="site-footer">
-        <div class="site-info">
+        <div class="site-info testing">
             <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
         </div>
     </footer>
